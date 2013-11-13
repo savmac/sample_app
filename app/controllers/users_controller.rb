9757ab5a8a5665b9997926a79a0e4903b30fe5b7
Scott Knight <scotteknight@gmail.com>
2013-11-12 21:57:14 -0800

    Add micropost controller and form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,13 +56,6 @@ class UsersController < ApplicationController
 
     # Before filters
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in"
-      end
-    end
-
     def signed_in_restrict
       redirect_to(root_url) if signed_in?
     end
